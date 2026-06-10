4646efb02a3c9020f837dbee13b6ef8857a17c82
AfMah-eng005 <Mahak_enginsoft@hotmail.com>
2026-06-10 03:14:30 -0600

    Added telegram API integration

--- alert_button.py
+++ alert_button.py
@@ -1,5 +1,11 @@
 import time
+import requests
 import RPi.GPIO as GPIO
+
+BOT_TOKEN = "8649277640:AAHGa4XFNaa4UDdvRVGsIhYyd3ue2tHoIjA"
+CHAT_ID = "8932215700"
+URL = f"https://api.telegram.org/bot8649277640:AAHGa4XFNaa4UDdvRVGsIhYyd3ue2tHoIjA/sendMessage"
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
@@ -8,8 +14,18 @@ button_pressed = False
 while True:
     if GPIO.input(7) == GPIO.HIGH and not button_pressed:
         print("Someone pressed the alert button!")
+
+        # Send Telegram message
+        data = {
+            "chat_id": CHAT_ID,
+            "text": "Someone pressed the alert button!"
+        }
+        requests.post(URL, json=data)
+
         button_pressed = True
+
     elif GPIO.input(7) == GPIO.LOW:
         button_pressed = False
+
     time.sleep(0.1)
 
